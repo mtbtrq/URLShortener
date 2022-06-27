9101b7d98c9b7e379c58992a781437452ade63e9
Mutayyab Tariq <83178953+mutyyab@users.noreply.github.com>
2022-06-27 05:11:51 -0700

    FML

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,8 +2,5 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import { register } from './serviceWorkerRegistration';
 
 ReactDOM.render(<App />, document.getElementById("root"));
-
-register();
