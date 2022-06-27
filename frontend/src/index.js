6b29348d07374898b9446b48b8ad36284af86f6b
Mutayyab Tariq <83178953+mutyyab@users.noreply.github.com>
2022-06-27 04:57:19 -0700

    Made website a PWA.

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,5 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
+import { register } from './serviceWorkerRegistration';
 
 ReactDOM.render(<App />, document.getElementById("root"));
+
+register();
